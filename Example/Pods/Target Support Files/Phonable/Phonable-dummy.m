2073923f3958efb444b5c2a47740090aaa5726ef
Can Koç <can-koc@users.noreply.github.com>
2019-11-01 15:43:25 +0300

    init

---
+++ Example/Pods/Target Support Files/Phonable/Phonable-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Phonable : NSObject
+@end
+@implementation PodsDummy_Phonable
+@end
---
+++ Example/Pods/Target Support Files/Pods-Phonable_Example/Pods-Phonable_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Phonable_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Phonable_Example
+@end
